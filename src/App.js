86a6c62f2203357e3ee3963f9844e75fe69f66a7
sawa-mon <69334235+sawa-mon@users.noreply.github.com>
2020-10-07 10:17:51 +0900

    Revert "Feature/re start"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { TodoForm } from './components/TodoForm';
-import { TodoList } from './components/TodoList';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [tasks,setTasks] = React.useState([]);
-
-// console.log(tasks)
-
-    return (
-      <div className='App'>
-        <h2>マイタスク</h2>
-        <TodoForm tasks={tasks} setTasks={setTasks}/>
-        <TodoList tasks={tasks} setTasks={setTasks} />
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
